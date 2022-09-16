4fefb751e8043b3cab39e6a5a514b29ca14716f7
Dragon <mkmaster@gmail.com>
2022-09-16 20:59:10 +0300

    Add IFlex interface

---
+++ FlexConfig/Interfaces/IFlex.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="IFlex.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace FlexConfig.Interfaces;
+
+/// <summary>
+/// Reference wrapper interface.
+/// </summary>
+public interface IFlex
+{
+    /// <summary>
+    /// Gets or sets stored value.
+    /// </summary>
+    public dynamic Value { get; set; }
+
+    /// <summary>
+    /// Gets the type of stored value.
+    /// </summary>
+    public Type Type { get; }
+}
